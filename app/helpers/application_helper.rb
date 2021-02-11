ada49c951e822e12aa6d4edd40c076b50c0ea393
Aidin Mohandesi <Aidin.mohandesi@gmail.com>
2021-02-11 18:43:20 +0330

    add custom loggers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -141,4 +141,16 @@ module ApplicationHelper
     return "None" if date.nil? # Handle invalid dates
     local_time(date, "%b %d, %Y %H:%M")
   end
+
+  def get_log_tag()
+    "[#{request.env["HTTP_X_FORWARDED_FOR"]}] [#{current_user.email}]"
+  end
+
+  def log_error(msg)
+    logger.error "#{get_log_tag} #{msg}"
+  end
+
+  def log_info(msg)
+    logger.info "#{get_log_tag} #{msg}"
+  end
 end
